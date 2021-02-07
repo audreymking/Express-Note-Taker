5f1008517675229c8493ea05fb0d64cb364b30fe
Audrey King <audreymking1@gmail.com>
2021-02-06 19:51:03 -0600

    added comments and correct console logs for walkthrough

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,16 +6,16 @@ const { v4: uuidv4 } = require("uuid");
 const promisifiedRead = util.promisify(fs.readFile);
 const promisifiedWrite = util.promisify(fs.writeFile);
 
-//get notes
+//get notes from array
 const getNotes = () => {
   return promisifiedRead("db/db.json", "utf8").then((notes) => {
     return JSON.parse(notes);
   });
 };
 
-//add notes to
+//add notes
 const addNote = (note) => {
-  //odify the notes to have id
+  //gives individual notes an id
   const newNote = note;
   newNote.id = uuidv4();
 
@@ -27,6 +27,7 @@ const addNote = (note) => {
   });
 };
 
+//removes id of removed notes
 const destroyNote = (id) => {
   return getNotes().then((notes) => {
     const allNotes = notes;
@@ -35,23 +36,29 @@ const destroyNote = (id) => {
   });
 };
 
+//connects express to router
 const router = require("express").Router();
 
+//get notes from router
 router.get("/api/notes", (req, res) => {
   getNotes().then((notes) => {
     res.json(notes);
   });
 });
 
+//post notes to router
 router.post("/api/notes", (req, res) => {
   addNote(req.body).then((notes) => {
     res.json(notes);
+    console.log("Saving note to db" + req.body.id);
   });
 });
 
+//delete notes from router
 router.delete("/api/notes/:id", (req, res) => {
   destroyNote(req.params.id).then((notes) => {
     res.json(notes);
+    console.log("Deleing note with id " + req.params.id);
   });
 });
 
